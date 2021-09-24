205ff1783f15d09bb02c8cb6f678bc245819dc70
MitchellSparrow <mitch.sparrow@hotmail.com>
2021-09-24 13:19:54 +0100

    added the about page and menu

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
-from flask import Flask, render_template
-from views import views 
+from flask import Flask, render_template,request
+import pandas as pd
+from data import get_f1_data
 
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix = "/views")
+
+
+@app.route("/")
+def home():
+    x = pd.DataFrame()
+    return render_template("index.html", name="Fantasy F1 Best Teams", data=x.to_html(classes='minimalistBlack'))
+
+@app.route("/about/")
+def about():
+    x = pd.DataFrame()
+    return render_template("about.html", name="About")
+
+@app.route('/analysis/', methods=['GET','POST'])
+def analysis():
+    try:
+        cost = request.form['budget']
+        df = get_f1_data(float(cost)).reset_index(drop=True)
+        df.index += 1 
+        x = df.head(30).round(2)
+        # x = pd.DataFrame(np.random.randn(20, 5))
+    except:
+        x = pd.DataFrame()
+        
+    return render_template("index.html", name="Fantasy F1 Best Teams", data=x.to_html(classes='minimalistBlack'))
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
--- views.py
+++ views.py
@@ -9,7 +9,12 @@ views = Blueprint(__name__,"views")
 @views.route("/")
 def home():
     x = pd.DataFrame()
-    return render_template("index2.html", name="Fantasy F1 Best Teams", data=x.to_html(classes='minimalistBlack'))
+    return render_template("index.html", name="Fantasy F1 Best Teams", data=x.to_html(classes='minimalistBlack'))
+
+@views.route("/about/")
+def about():
+    x = pd.DataFrame()
+    return render_template("about.html", name="About")
 
 @views.route('/analysis/', methods=['GET','POST'])
 def analysis():
@@ -17,9 +22,9 @@ def analysis():
         cost = request.form['budget']
         df = get_f1_data(float(cost)).reset_index(drop=True)
         df.index += 1 
-        x = df.head(30)
+        x = df.head(30).round(2)
         # x = pd.DataFrame(np.random.randn(20, 5))
     except:
         x = pd.DataFrame()
         
-    return render_template("index2.html", name="Fantasy F1 Best Teams", data=x.to_html(classes='minimalistBlack'))
+    return render_template("index.html", name="Fantasy F1 Best Teams", data=x.to_html(classes='minimalistBlack'))
