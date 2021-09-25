44951c5a94cfad32e9625c2a54019f59b4dc934c
MitchellSparrow <mitch.sparrow@hotmail.com>
2021-09-25 10:47:27 +0100

    added form check, and updated scrolling for table on mobile

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,9 +1,34 @@
 
+$(window).on('load', function(){ 
+    $('#loading').hide();
+});
 
+function checkInp(){
+    console.log("hello");
+  var x=document.forms["budget_form"]["budget"].value;
+  if (isNaN(x)) 
+  {
+    console.log("hello");
+    alert("Your budget must be a number!");
+    return false;
+  }
+  $('#loading').show();
+}
 
+$("form").submit(function (e) {
+    var validationFailed = false;
+    // validation
+    if (isNaN( $("#search").val()  )|| $("#search").val() == null || $("#search").val() == "") {
+        validationFailed = true;
+    } 
 
-
-    $(window).on('load', function(){ 
+    if (validationFailed) {
         $('#loading').hide();
-        console.log("finished loading");
-    });
+        alert('Please enter your budget as a number!');
+        e.preventDefault();
+        return false;
+    }
+ }); 
+
+
+ 
